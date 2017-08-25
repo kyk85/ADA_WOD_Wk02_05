796bf3e11964bf8440713a4a2b504d1fc0968e14
KYK <kay2kay1@gmail.com>
2017-08-26 00:03:35 +0800

    Fixed bugs

--- ADA_WOD_Wk02_05.js
+++ ADA_WOD_Wk02_05.js
@@ -1,18 +1,20 @@
 var msg=''
 
 function factorial(){
+	msg=''							//clears msg
 	var x=document.getElementById('numOne').value;
 	document.getElementById('factorial').innerHTML="The factorial of " + x + " is:"
-	var y=x
+	var y=x					//declare new variable as proxy of x (so that x is not affected by calculation)
 	msg += x
 	for (i=1;i<=x;x--){
 		if (x!==i){		
 		msg += " x" + " " + (x-1);
-		y=y*(x-1);
+		y=y*(x-1);					//calculates then redeclares y for next iteration of loop
 		}
 	}
 	
 	document.getElementById('answer').innerHTML=msg + " = " + y
+	document.getElementById('numOne').value=''						//clears input field
 }
 
 //factorial(10);
